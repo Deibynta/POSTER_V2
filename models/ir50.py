b120142f6a04f6bf0813fd88a325cfea73d9eb40
Deibynta <149353184+Deibynta@users.noreply.github.com>
2025-03-25 14:20:12 +0530

    Update ir50.py

--- models/ir50.py
+++ models/ir50.py
@@ -301,17 +301,19 @@ class Backbone(Module):
 
 
 class ChannelAttention(Module):
-    def __init__(self, in_planes, ratio=16):
-        in_planes=64
-        print("CBAM")
-        super(ChannelAttention, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.max_pool = nn.AdaptiveMaxPool2d(1)
-           
-        self.fc = nn.Sequential(nn.Conv2d(in_planes, in_planes // 16, 1, bias=False),
-                               nn.ReLU(()),
-                               nn.Conv2d(in_planes // 16, in_planes, 1, bias=False))
+    def __init__(self,input_nc,rotio = 16):
+        super().__init__()
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.maxpool = nn.AdaptiveMaxPool2d(1)
+        self.share_mlp = nn.Sequential(nn.Conv2d(in_channels=input_nc,out_channels=input_nc//rotio,kernel_size=1,bias=False),
+                                      nn.ReLU(),
+                                      nn.Conv2d(in_channels=input_nc//rotio,out_channels=input_nc,kernel_size=1,bias=False))
         self.sigmoid = nn.Sigmoid()
+        
+    def forward(self,x):
+        avg_x = self.share_mlp(self.avgpool(x))
+        max_x = self.share_mlp(self.maxpool(x))
+        return self.sigmoid(avg_x+max_x)
 
     def forward(self, x):
         print("CBAM 1: ",x.shape)
@@ -323,20 +325,20 @@ class ChannelAttention(Module):
         return self.sigmoid(out)
 
 class SpatialAttention(Module):
-    def __init__(self, kernel_size=7):
-        super(SpatialAttention, self).__init__()
-
-        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=kernel_size//2, bias=False)
+    def __init__(self,kernel=7):
+        super().__init__()
+        assert kernel in (3,7),"Kernel size must be in 3 or 7"
+        padding = 1 if kernel == 3 else 3
+        self.cnn = nn.Conv2d(2,1,kernel_size=kernel,padding=padding,bias=False)
         self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        avg_out = torch.mean(x, dim=1, keepdim=True)
-        max_out, _ = torch.max(x, dim=1, keepdim=True)
-        x = torch.cat([avg_out, max_out], dim=1)
-        x = self.conv1(x)
+    def forward(self,x):
+        avg_out = torch.mean(x,dim=1,keepdim=True)
+        max_out = torch.max(x,dim=1,keepdim=True).values
+        x = torch.cat([avg_out,max_out],dim=1)
+        x = self.cnn(x)
         return self.sigmoid(x)
         
-class cbam(nn.Module):
+class cbam(Module):
     def __init__(self,input_nc):
         super().__init__()
         self.ca = ChannelAttention(input_nc)
